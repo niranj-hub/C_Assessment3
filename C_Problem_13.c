da3e2073676deb6ee943d4a5c9a6c605684c0dd5
Niranjana <ponniranjana2006@gmail.com>
2026-06-03 17:29:09 +0530

    Create C_Problem_13.c

---
+++ C_Problem_13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int a,ones,tens;
+    printf("Enter a four digit number:");
+    scanf("%d",&a);
+    ones=a%10;
+    tens=(a/10)%10;
+    if (ones==tens)
+      printf("Success");
+    else
+      printf("Failure");
+    return 0;
+}
